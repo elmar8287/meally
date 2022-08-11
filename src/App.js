eb195a2e18261d4bba873f0e57f620fcff9a9017
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2022-08-11 10:12:28 +0400

    Finished filtering logic

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ const App = () => {
   const [modal, setModal] = useState(false)
   const [addInfo, setAddInfo] = useState("")
 
+  const [option, setOption] = useState("")
+  const optionHandle = (e) => {
+    setOption(e.target.value)
+  }
+
   // const chickenHandle = () => {
   //   setInput("chicken")
   // }
@@ -27,12 +32,12 @@ const App = () => {
 
   useEffect(()=> {
     axios
-      .get("https://themealdb.com/api/json/v1/1/filter.php?i="+input)
+      .get("https://themealdb.com/api/json/v1/1/filter.php?c="+option)
       .then((resp)=>{
         setCat(resp.data)
         console.log("cat", cat)
       })
-  },[input])
+  },[option])
   console.log("after useEffect",cat)
 
   const addInfoHandle = (e) => {
@@ -45,12 +50,38 @@ const App = () => {
     setModal(!modal)
   }
 
+
   if (cat===null) {
     <p>Loading</p>
   } else {
   return (
     <div className="main">
-          <Filter />
+          
+
+          <div className="filtering">
+      <h3 className="cat-title">Select the category</h3>
+      <div className="cat-section">
+      <select name="categories" className="categories" onChange={optionHandle}>
+        <option value="beef">Beef</option>
+        <option value="breakfast">Breakfast</option>
+        <option value="chicken">Chicken</option>
+        <option value="dessert">Dessert</option>
+        <option value="Goat">Goat</option>
+        <option value="lamb">Lamb</option>
+        <option value="miscellaneous">Miscellaneous</option>
+        <option value="pasta">Pasta</option>
+        <option value="pork">Pork</option>
+        <option value="seafood">Seafood</option>
+        <option value="side">Side</option>
+        <option value="starter">Starter</option>
+        <option value="vegan">Vegan</option>
+        <option value="vegetarian">Vegetarian</option>
+    </select>
+      </div>
+    </div>
+
+
+
       <div className="meals">
               {/* <div>Found {cat.meals.length} reciepts</div> */}
               {console.log("inside of return", cat)}
